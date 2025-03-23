dff7c6bb559bd97aaefa09d0d851a35e62b0e8c7
Eden Reich <eden.reich@gmail.com>
2025-03-23 18:37:58 +0000

    refactor: Clarify TODO comment regarding error event type in GenerateContentStream method
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- sdk.go
+++ sdk.go
@@ -265,7 +265,7 @@ func (c *clientImpl) GenerateContentStream(ctx context.Context, provider Provide
 					errorData := []byte(fmt.Sprintf(`{"error": "%s"}`, err.Error()))
 					eventChan <- SSEvent{
 						Event: nil,
-						Data:  &errorData, // TODO - need to add error event type to enum in OpenAPI spec, but it's not very important because all providers following OpenAI and the event section is not provided in the SSEvents
+						Data:  &errorData, // TODO - need to add error event type to enum in OpenAPI spec, but it's not very important because all providers following OpenAI and the event section is not provided in the SSEvents, i.e it's an optional and will be treated as a "nice-to-have"
 					}
 				}
 				return
